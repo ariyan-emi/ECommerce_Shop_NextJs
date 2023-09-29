30ac9e1f7205ed0d9ff054f6c0766b2a3eda331f
WebVaVe <emami8637@gmail.com>
2023-09-29 06:25:29 +0330

    Debug Building Project✨

--- components/Cart/Cart.tsx
+++ components/Cart/Cart.tsx
@@ -1,18 +1,11 @@
 'use client'
+import {LocalStorage} from "../Service/LocalStorage";
 import DeleteIcon from '@mui/icons-material/Delete';
 import {EmptyCart} from "./EmptyCart";
 
 export function Cart() {
-
-    let count = JSON.parse(window.localStorage.getItem('ShoppingCard') || '{}')
-    const nonDuplicatedData:any = [];
-    count.map((x:any) => {
-        if (!nonDuplicatedData[x.id]){
-            nonDuplicatedData[x.id] = x;
-        }
-    });
-    const filteredData = nonDuplicatedData.filter((n:any)  => {return n != undefined});
-    let GetItemFromStorage = filteredData;
+    LocalStorage()
+    let GetItemFromStorage = LocalStorage();
     return (
         <>
 
