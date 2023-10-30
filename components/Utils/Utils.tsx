4f1ea0ced533a7ea1f58735498c4fd9f6c01a35a
WebVaVe <emami8637@gmail.com>
2023-10-30 16:10:06 +0330

    Add UI Auth Page✨

--- components/Utils/Utils.tsx
+++ components/Utils/Utils.tsx
@@ -1,36 +1,8 @@
 'use client'
 import swal from 'sweetalert';
-import { useState, useEffect } from 'react';
 export function GetTaxes() {
     return Number(1.99)
 }
 export function ShowAlert(title:string, discription:string, icon:string){
     swal(title, discription, icon);
 }
-
-type WindowDimentions = {
-    width: number | undefined;
-    height: number | undefined;
-};
-
-const useWindowDimensions = (): WindowDimentions => {
-    const [windowDimensions, setWindowDimensions] = useState<WindowDimentions>({
-        width: undefined,
-        height: undefined,
-    });
-    useEffect(() => {
-        function handleResize(): void {
-            setWindowDimensions({
-                width: window.innerWidth,
-                height: window.innerHeight,
-            });
-        }
-        handleResize();
-        window.addEventListener('resize', handleResize);
-        return (): void => window.removeEventListener('resize', handleResize);
-    }, []); // Empty array ensures that effect is only run on mount
-
-    return windowDimensions;
-};
-
-export default useWindowDimensions;
