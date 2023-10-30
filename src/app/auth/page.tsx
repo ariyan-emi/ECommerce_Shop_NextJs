4af6aac7986690214d7b505d0d847a2b5a5283bb
WebVaVe <emami8637@gmail.com>
2023-10-30 16:21:09 +0330

    Add UI Auth Page✨

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -4,14 +4,17 @@ import {AuthPhone} from "../../../components/Auth/AuthPhone";
 import {useState} from "react";
 import {Loading} from "../../../components/Screens/Loading";
 export default function Home() {
+
     const [isLoading, setIsLoading] = useState(true);
     setTimeout(() => {
         setIsLoading(false)
     }, 500);
     if (!isLoading){
-
+        if (true){
             return(<AuthDesktop/>)
-
+        }else {
+            return(<AuthPhone/>)
+        }
     }
     else {
         return(<Loading/>)
