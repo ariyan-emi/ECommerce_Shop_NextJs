cf48f708f0ed62f3856c0d584f7a0b9602f456b8
WebVaVe <emami8637@gmail.com>
2023-11-15 03:03:43 +0330

    Upgrade to Firebase database Best Version (Debug) 😲✨

--- components/Cart/Cart.tsx
+++ components/Cart/Cart.tsx
@@ -1,23 +1,63 @@
-
+import {EmptyCart} from "./EmptyCart";
 import {useState} from "react";
+import {ShoppingCard} from "./ShoppingCard";
+import {Checkout} from "./Checkout";
+import {Invoice} from "./Invoice";
 import {useSelector} from "react-redux";
+import {SetData} from "../Utils/SetData";
 import {getIsAuth} from "../Redux/slices/isAuthSlice";
 import {useRouter} from "next/navigation";
-
-
 
 export function Cart() {
     let [showComponent,setShowComponent] =useState("cart")
     const isAuth = useSelector(getIsAuth);
-    const router = useRouter()
-    if (!isAuth){
-        router.push('/auth')
-    }
+    console.log(isAuth)
     return (
 
         <>
+            <SetData/>
+            {(() => {
+                 if (showComponent == "cart") {
+                    return (
+                        <>
+                            <ShoppingCard Component={showComponent} setComponent={setShowComponent}/>
+                        </>
+                    )
+                }else if (showComponent == "checkout") {
+                    return (
+                        <>
+                            <Checkout Component={showComponent} setComponent={setShowComponent}/>
+                        </>
+                    )
+                }else if (showComponent == "invoice") {
+                    return (
+                        <>
+                            <Invoice Component={showComponent} setComponent={setShowComponent}/>
+                        </>
+                    )
+                }else if (showComponent == "empty") {
+                    return (
+                        <EmptyCart/>
+                    )
+                }else {
+                     return (
+                         <EmptyCart/>
+                     )
+                 }
+            })()}
+            <style jsx>{`
+              .visibleClass {
+                animation: OpenCart 1s ease-out forwards;
+              }
 
-
+              @keyframes OpenCart {
+                0% {
+                  opacity: 0.4
+                }
+                100% {
+                  opacity: 1
+                }
+              }`}</style>
         </>
 
     )
