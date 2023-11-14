4132609f37f2aa9d4753efa15611bf54f109d9ea
WebVaVe <emami8637@gmail.com>
2023-11-15 02:58:34 +0330

    Upgrade to Firebase database Best Version (Debug) 😲✨

--- components/Cart/Cart.tsx
+++ components/Cart/Cart.tsx
@@ -1,18 +1,10 @@
-'use client'
+
 import {useState} from "react";
-import {useSelector} from "react-redux";
-import {getIsAuth} from "../Redux/slices/isAuthSlice";
-import {useRouter} from "next/navigation";
 
 
 
 export function Cart() {
     let [showComponent,setShowComponent] =useState("cart")
-    const isAuth = useSelector(getIsAuth);
-    const router = useRouter()
-    if (!isAuth){
-        router.push('/auth')
-    }
     return (
 
         <>
