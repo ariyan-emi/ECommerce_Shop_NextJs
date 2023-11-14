c7dbefba104981402ed78bbbd0db104f062038e0
WebVaVe <emami8637@gmail.com>
2023-11-15 02:02:43 +0330

    Upgrade to Firebase database Best Version (Debug) 😲✨

--- components/Cart/Cart.tsx
+++ components/Cart/Cart.tsx
@@ -1,12 +1,6 @@
 'use client'
-import {EmptyCart} from "./EmptyCart";
 import {useState} from "react";
-import {ShoppingCard} from "./ShoppingCard";
-import {Checkout} from "./Checkout";
-import {Invoice} from "./Invoice";
-import {Provider, useSelector} from "react-redux";
-import store from "../Redux/store";
-import {SetData} from "../Utils/SetData";
+import {useSelector} from "react-redux";
 import {getIsAuth} from "../Redux/slices/isAuthSlice";
 import {useRouter} from "next/navigation";
 
@@ -20,49 +14,10 @@ export function Cart() {
         router.push('/auth')
     }
     return (
-        <>
-            {(() => {
-                 if (showComponent == "cart") {
-                    return (
-                        <>
-                            <ShoppingCard Component={showComponent} setComponent={setShowComponent}/>
-                        </>
-                    )
-                }else if (showComponent == "checkout") {
-                    return (
-                        <>
-                            <Checkout Component={showComponent} setComponent={setShowComponent}/>
-                        </>
-                    )
-                }else if (showComponent == "invoice") {
-                    return (
-                        <>
-                            <Invoice Component={showComponent} setComponent={setShowComponent}/>
-                        </>
-                    )
-                }else if (showComponent == "empty") {
-                    return (
-                        <EmptyCart/>
-                    )
-                }else {
-                     return (
-                         <EmptyCart/>
-                     )
-                 }
-            })()}
-            <style jsx>{`
-              .visibleClass {
-                animation: OpenCart 1s ease-out forwards;
-              }
 
-              @keyframes OpenCart {
-                0% {
-                  opacity: 0.4
-                }
-                100% {
-                  opacity: 1
-                }
-              }`}</style>
+        <>
+
+
         </>
 
     )
