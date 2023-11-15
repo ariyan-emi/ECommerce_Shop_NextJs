b28b26e53e1d98c499f4fef0e29c32a22536543f
WebVaVe <emami8637@gmail.com>
2023-11-15 16:31:30 +0330

    Upgrade to Firebase database Best Version😲✨

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -2,18 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBgnGkG2wlwyh3wKyi10OnqvJiJKmbXKEY",
-  authDomain: "nextjsecommerce-c8701.firebaseapp.com",
-  databaseURL: "https://nextjsecommerce-c8701-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "nextjsecommerce-c8701",
-  storageBucket: "nextjsecommerce-c8701.appspot.com",
-  messagingSenderId: "407240513288",
-  appId: "1:407240513288:web:a03bd31c13287f570afebd",
-  measurementId: "G-TFHK7746RP"
+  apiKey: process.env.NEXT_PUBLIC_APP_API_KEY,
+  authDomain:process.env.NEXT_PUBLIC_APP_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_APP_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_APP_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_APP_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_APP_ID,
+  measurementId:process.env.NEXT_PUBLIC_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
