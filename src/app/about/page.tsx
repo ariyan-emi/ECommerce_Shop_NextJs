694d8309a3ebaca65a0289a076278fef5e63655d
WebVaVe <emami8637@gmail.com>
2023-12-01 21:26:49 +0330

    Add About Us💖✨

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,14 @@
+'use client'
 import {About} from "../../../components/Screens/About";
+import {SetData} from "../../../components/Utils/SetData";
+import {Provider} from "react-redux";
+import store from "../../../components/Redux/store";
 
 export default function Home() {
   return (
-    <About/>
+      <Provider store={store}>
+        <SetData/>
+        <About/>
+      </Provider>
   )
 }
